0d7eb263594c6973b5bb03223f27bc8340d70fc5
dmitri4k <mdsunknown@gmail.com>
2023-05-06 12:10:06 +0300

    feat: add 05 solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+
+const BUNDLE_NAME = 'bundle.css';
+const DIST_DIRECTORY = 'project-dist';
+const STYLES_DIRECTORY = 'styles';
+const CSS_EXT = '.css';
+
+const bundleFilePath = path.join(__dirname, DIST_DIRECTORY, BUNDLE_NAME);
+const stylesDirPath = path.join(__dirname, STYLES_DIRECTORY);
+
+async function getFilePaths(dirPath, ext) {
+  const readDir = async (dir) => {
+    const files = await fsPromises.readdir(dir, { withFileTypes: true });
+    const paths = await Promise.all(files.map(async (file) => {
+      const filePath = path.join(dir, file.name);
+      return file.isDirectory()
+        ? readDir(filePath)
+        : (path.extname(file.name) === ext) ? [ filePath ] : [];
+    }));
+    return paths.reduce((acc, path) => [...acc, ...path], []);
+  };
+  return await readDir(dirPath);
+}
+
+async function makeStyleBundle(dir, bundle) {
+  const cssFilePaths = await getFilePaths(dir, CSS_EXT);
+  const writer = fs.createWriteStream(bundle);
+  for (const path of cssFilePaths) {
+    const reader = fs.createReadStream(path);
+    await new Promise((resolve, reject) => {
+      reader.pipe(writer, { end: false }).on('error', (err) => reject(err));
+      reader.on('end', resolve);
+    });
+  }
+  writer.close();
+}
+
+makeStyleBundle(stylesDirPath, bundleFilePath);
